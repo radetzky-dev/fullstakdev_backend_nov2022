5af6effc7111ee0e3a16f1739678f5ad9b73d47a
paolo <paolo.roversi@gmail.com>
2022-11-24 20:00:39 +0100

    date time esempio

---
+++ lezione2/daytime.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo "This is the page title"; ?></title>
+</head>
+<body>
+    <p>
+    TIME: <?php echo date('H:i'); ?> <br>DAY <?php echo date('d/m/Y');
+  ?>
+
+    </p>
+</body>
+</html>
