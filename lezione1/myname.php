144c12e93daa42cbe031e0b9aecc028ba9968a1b
paolo <paolo.roversi@gmail.com>
2022-11-24 08:31:45 +0100

    nuovo file di test

---
+++ lezione1/myname.php
@@ -0,0 +1,16 @@
+<?php
+/*
+This is a comment multi
+lines
+*/
+
+// http://localhost/musa/fullstackdev/fullstakdev_backend_nov2022/lezione1/myname.php
+// this is a single line comment
+# another comment type
+$name = "Paolo";
+echo "Il mio nome è " . $name;
+function print_my_name($name) {
+    echo " <strong>Hello " . $name . "!</strong>";
+}
+print_my_name($name);
+?>
