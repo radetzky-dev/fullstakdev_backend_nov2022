844ec8d662a18d8e777a82f698bc7f94d67e8b4b
zhiti88 <118974843+zhiti88@users.noreply.github.com>
2022-11-29 19:42:53 +0100

    prima modifica

--- helloworld.php
+++ helloworld.php
@@ -4,7 +4,8 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
- <?php echo ' <h1> questa e un amodifica </h1>' ?>
+ <?php echo ' <h1> questa e un amodifica </h1>' ;?>
+ <?php echo '<h1> questa e la prima cartella su github </h1>';?>
  </body>
 
 </html>
