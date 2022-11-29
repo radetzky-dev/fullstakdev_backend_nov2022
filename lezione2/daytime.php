ca1ece7cbe287dbc847c6717fcb54cf4d500c034
paolo <paolo.roversi@gmail.com>
2022-11-29 18:48:10 +0100

    inserito hello world

--- lezione2/daytime.php
+++ lezione2/daytime.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="ita">
+
 <head>
-    <title><?php echo "This is the page title"; ?></title>
+  <title><?php echo "This is the page title"; ?></title>
 </head>
+
 <body>
-    <p>
+  <p>
     TIME: <?php echo date('H:i'); ?> <br>DAY <?php echo date('d/m/Y');
-  ?>
-    </p>
+                                              ?>
+    <?php echo "Hello world"; ?>
+  </p>
 </body>
+
 </html>
