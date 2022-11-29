c1ca01a1f2660ae62d25cd93da3904d1e0f99e13
paolo <paolo.roversi@gmail.com>
2022-11-29 20:41:03 +0100

    Il mio primo Array

---
+++ lezione3/myfirstArray.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>La mia auto</title>
+</head>
+
+<body>
+    <h1>AUTOMOBILI</h1>
+    <?php
+
+    $auto = array("Ferrari", "Fiat", "Lancia", "Alfa Romero");
+
+    //var_dump($auto);
+
+    //var_dump($auto[2]);
+
+    echo "<p>Ho appena comprato una " . $auto[2] . "</p>";
+
+    ?>
+</body>
+
+</html>
