5d27901732cddd3cdb538839a4329d24924eb533
paolo <paolo.roversi@gmail.com>
2022-11-29 20:56:24 +0100

    vari tipi di array

--- lezione3/myfirstArray.php
+++ lezione3/myfirstArray.php
@@ -10,12 +10,28 @@
     <?php
 
     $auto = array("Ferrari", "Fiat", "Lancia", "Alfa Romero");
+    $auto[4] = "Lamborghini";
 
-    //var_dump($auto);
+    $auto[2] = "Aston Martin";
+
+    var_dump($auto);
 
     //var_dump($auto[2]);
 
     echo "<p>Ho appena comprato una " . $auto[2] . "</p>";
+
+    $eta = array ('Mario' => 38, "Piero" =>35, "Gino" => "81");
+
+    var_dump($eta);
+
+    echo '<br>'.$eta['Mario'];
+
+    $famiglia = array (
+        "Rossi" => array ("A","b","c"),
+        "Verdi" => array ("A","b","c"),
+    );
+
+    var_dump($famiglia);
 
     ?>
 </body>
