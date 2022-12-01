d59a8e3935f31acde66d0e43afd01ad7adedd56c
paolo <paolo.roversi@gmail.com>
2022-12-01 18:56:50 +0100

    assegnazione artmetica

---
+++ lezione3/esempi_vari_assegnazioni.php
@@ -0,0 +1,40 @@
+<?php
+
+$x= "hello world";
+$x = null;
+
+//esempio di null
+var_dump($x);
+
+//esempi operatori aritmetici
+$a = 5;
+$b = 7;
+echo 'Le mie varibili<br>';
+echo "a = $a  e b = $b <br><hr>";
+echo 'a = '.$a.'  e b = '.$b. '<br><hr>';
+
+echo '<br>Somma<br>';
+echo $a+$b;
+echo '<br>Sottrazione<br>';
+echo $a-$b;
+echo '<br>Moltiplicazione<br>';
+echo $a*$b;
+echo '<br>Divisione<br>';
+echo $a/$b;
+echo '<br>Modulo <br>';
+echo (5 % 3);
+echo '<br>Incremento<br>';
+$a++;
+$b++;
+echo "a = $a  e b = $b <br><hr>";
+echo '<br>Sottraggo<br>';
+$a--;
+$b--;
+echo "a = $a  e b = $b <br><hr>";
+
+
+
+
+
+
+
