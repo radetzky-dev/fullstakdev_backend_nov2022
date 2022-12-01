d2bdaadb372f3eb6a0138323c1224ad8bafac1e8
paolo <paolo.roversi@gmail.com>
2022-12-01 20:57:34 +0100

    incrementi

--- lezione3/esempi_vari_assegnazioni.php
+++ lezione3/esempi_vari_assegnazioni.php
@@ -100,3 +100,24 @@ var_dump($e);
 $f = ! (true);
 echo 'f '.$f.'<br>';
 var_dump($f);
+
+echo '<hr>';
+echo "<h3>Postincrement</h3>";
+$a = 5;
+echo "Should be 5: " . $a++ . "<br />\n";
+echo "Should be 6: " . $a . "<br />\n";
+
+echo "<h3>Preincrement</h3>";
+$a = 5;
+echo "Should be 6: " . ++$a . "<br />\n";
+echo "Should be 6: " . $a . "<br />\n";
+
+echo "<h3>Postdecrement</h3>";
+$a = 5;
+echo "Should be 5: " . $a-- . "<br />\n";
+echo "Should be 4: " . $a . "<br />\n";
+
+echo "<h3>Predecrement</h3>";
+$a = 5;
+echo "Should be 4: " . --$a . "<br />\n";
+echo "Should be 4: " . $a . "<br />\n";
