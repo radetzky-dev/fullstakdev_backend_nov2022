e34bd4659f87084e0057c7c681be032322cf046a
paolo <paolo.roversi@gmail.com>
2022-12-15 19:54:26 +0100

    delete

--- lezione6/ancoraarray.php
+++ lezione6/ancoraarray.php
@@ -1,8 +1,8 @@
 <?php
 
-$myCars = array ('bmw','mercedes','fiat');
+$myCars = array('bmw', 'mercedes', 'fiat');
 
-$myOtherCars = ['jaguar','peugeot','ferrari'];
+$myOtherCars = ['jaguar', 'peugeot', 'ferrari'];
 
 var_dump($myCars);
 echo '<hr>';
@@ -20,7 +20,7 @@ $frutti = [
 unset($frutti['uno']);
 
 
-$test = ['1','1','2','1'];
+$test = ['1', '1', '2', '1'];
 
 var_dump($frutti);
 $frutti['tre'] = 'anguria';
@@ -35,25 +35,50 @@ var_dump($myCars);
 echo '<hr>';
 
 
+echo 'My car is a ' . $myCars[1] . '<br>';
 
-echo 'My car is a '.$myCars[1].'<br>';
-
-echo 'My car is a '.$frutti['uno'].'<br>';
+echo 'My car is a ' . $frutti['uno'] . '<br>';
 
 
 $name = 'Felipe';
 
-if(isset($name)) {
+if (isset($name)) {
     echo $name;
-	}
+}
 
 echo  '-';
 unset($name);
 
-if(isset($name)) {
+if (isset($name)) {
     echo $name;
-	}
-else
-{
-	echo 'la variabile non è più settata';
+} else {
+    echo 'la variabile non è più settata';
 }
+
+
+$MyArray = array('MON', 'TUE', 'WED');
+
+$toDelete = array('xxx', 'FRI');
+
+//using index number to assign values
+//to 4th and 5th elements of the array
+$MyArray[3] = 'THU';
+$MyArray[4] = 'xxx';
+$MyArray[5] = 'FRI';
+
+
+for ($i = 0; $i < count($MyArray); $i++) {
+    //access element using index number
+    echo "Value at position=$i is $MyArray[$i] <br>";
+
+    for ($x = 0; $x < count($toDelete); $x++) {
+        if (in_array($toDelete[$x], $MyArray)) {
+            echo $toDelete[$x] . ' presente!<br>';
+            $keyToDelete = array_search('xxx', $MyArray);
+            unset($MyArray[$keyToDelete]);
+        }
+    }
+}
+
+echo '<br>';
+var_dump($MyArray);
