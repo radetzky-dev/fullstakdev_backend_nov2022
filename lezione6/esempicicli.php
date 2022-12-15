59bc880ee109e79afbb26b950d26df1467fe670c
paolo <paolo.roversi@gmail.com>
2022-12-15 18:45:50 +0100

    cicli

---
+++ lezione6/esempicicli.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+- *
+- **
+- ***
+- ****
+- *****
+*/
+
+$hyphen = "-";
+$star = "*";
+$i = 1;
+while ($i <= 5) {
+    //echo $hyphen . " " . str_repeat($star, $i) . "<br>";
+            //ciclo for $i
+
+            echo $hyphen . " ";
+            for ($x = 1; $x <= $i; $x++) {
+                echo "*";
+            }
+            echo '<br>';
+    $i++;
+}
