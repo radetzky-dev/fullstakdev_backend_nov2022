81429b38a4b30e8fc1d535fc5d8128ea2b7299c6
paolo <paolo.roversi@gmail.com>
2022-12-15 18:52:33 +0100

    ok

--- lezione6/esempicicli.php
+++ lezione6/esempicicli.php
@@ -13,12 +13,13 @@ $star = "*";
 $i = 1;
 while ($i <= 5) {
     //echo $hyphen . " " . str_repeat($star, $i) . "<br>";
-            //ciclo for $i
+    //ciclo for $i
 
-            echo $hyphen . " ";
-            for ($x = 1; $x <= $i; $x++) {
-                echo "*";
-            }
-            echo '<br>';
+    echo $hyphen . " ";
+    for ($x = 1; $x <= $i; $x++) {
+        echo "*";
+    }
+    echo '<br>';
+    
     $i++;
 }
