9d9b7db66e942f74908aca23fd6d0fb5149d6599
paolo <paolo.roversi@gmail.com>
2022-12-15 21:30:23 +0100

    funzioni

---
+++ lezione6/prima_funzione.php
@@ -0,0 +1,29 @@
+<?php
+
+/** sum numbers */
+function sumNumbers(int $x, int $y): void
+{
+    $z = $x + $y;
+    echo "Sum of $x and $y is: $z <br>";
+}
+
+//array_search(mixed $needle, array $haystack, bool $strict = false): int|string|false
+
+/** sum numbers */
+function sumNumberSimple(int $x, int $y): int
+{
+    return $x + $y;
+}
+
+function somma($x, $y)
+{
+    return $x + $y;
+}
+
+$firstNumber = 15;
+$secondNumber = 10;
+sumNumbers($firstNumber, $secondNumber);
+
+echo sumNumberSimple($firstNumber, $secondNumber) . '<br>';
+
+echo somma($firstNumber, $secondNumber) . '<br>';
