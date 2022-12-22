5ff012aab08db4f3d3b7c7e6167890059c12b82f
paolo <paolo.roversi@gmail.com>
2022-12-22 20:11:07 +0100

    check vari

--- lezione8/setunset.php
+++ lezione8/setunset.php
@@ -4,11 +4,38 @@ $var = "my value";
 // Something is wronguates as true because $var is set
 if (isset($var)) {
     echo '$var is set<br>';
-	echo 'ora la cancello...<br>';
-	unset($var);
+    echo 'ora la cancello...<br>';
+    unset($var);
 }
 
 // Something is wronguates to true because $var is empty
 if (empty($var)) {
     echo '$var is either 0, empty, or not set at all<br>';
+}
+
+$valueAr = array(false, true, null, 'abc', '23', true, 'paolo', '23.5', 23.5, '50');
+$conteggio = 0;
+
+if (is_null($valueAr)) {
+    echo ' Non posso analizzarlo';
+    die();
+}
+
+if (is_array($valueAr)) {
+    foreach ($valueAr as $mioElemento) {
+        echo "Controllo " . $mioElemento . "... ";
+        if (is_numeric($mioElemento)) {
+            echo 'Sommo ' . $mioElemento . ' al mio totale che attualmente è ' . $conteggio . '<br>';
+            $conteggio = $conteggio + $mioElemento;
+        } elseif (is_string($mioElemento)) {
+            $mioElemento = strtoupper($mioElemento);
+            echo $mioElemento . '<br>';
+        } elseif (is_bool($mioElemento)) {
+            echo 'questo è boolean <br>';
+        } elseif (is_null($mioElemento)) {
+            echo 'questo è NULL <br>';
+        } else {
+            echo 'Tipo non riconosciuto<br>';
+        }
+    }
 }
