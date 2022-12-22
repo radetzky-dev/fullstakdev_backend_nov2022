5002e84d8dbf79b63c8e1c538eca3a9396450d0b
paolo <paolo.roversi@gmail.com>
2022-12-22 21:18:39 +0100

    soluzione1

--- lezione8/esercizi.php
+++ lezione8/esercizi.php
@@ -8,8 +8,37 @@ madam
 osso
 ossesso
 esose
+*/
 
+$array = ['madam', 'osso', 'ossesso', 'esose', 'pippo'];
 
+foreach ($array as $arr) {
+    $arr2 = strrev($arr);
+    if ($arr2 === $arr) {
+        echo $arr . " è palindroma <br>";
+    } else {
+        echo $arr . " non è palindroma <br>";
+    }
+}
+
+echo '<br>';
+
+function palindromo($string)
+{
+    $lowercaseString = strtolower($string);
+    $formattedString = str_replace(' ', '', $lowercaseString);
+    $reversedString = strrev($formattedString);
+
+    if ($reversedString === $formattedString) {
+        return $string . '  è palindroma<br>';
+    } else return $string . ' non è palindroma<br>';
+}
+
+foreach ($array as $arr) {
+    echo palindromo($arr);
+}
+
+/*
 2)
 Trovare il maggiore fra 3 numeri
 */
