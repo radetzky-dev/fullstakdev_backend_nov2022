b0abd4ffb4f6269f88fa4b18898f577a9a251fdf
paolo <paolo.roversi@gmail.com>
2023-01-10 20:03:26 +0100

    date

--- lezione9/esempi_date.php
+++ lezione9/esempi_date.php
@@ -42,3 +42,48 @@ $today = getdate();
 echo "<pre>";
 var_dump($today);
 echo "</pre>";
+echo '<br>';
+$Now = new DateTime('now', new DateTimeZone('Europe/Rome'));
+echo  $Now->format('d-m-Y H:i:s');
+
+echo 'Current time: ' . date('Y-m-d H:i:s') . "<br>";
+
+$format = 'Y-m-d';
+$date = DateTimeImmutable::createFromFormat($format, '2023-01-10');
+echo "Format: $format; " . $date->format('d-m-y H:i:s') . "<br>";
+
+date_default_timezone_set("Europe/Rome");
+echo 'Current time: ' . date('Y-m-d H:i:s') . "<br>";
+
+$format = 'Y-m-d';
+$date = DateTimeImmutable::createFromFormat($format, '2023-01-10');
+echo "Format: $format; " . $date->format('d-m-y H:i:s') . "<br>";
+
+//Set data e set ora
+$date = new DateTimeImmutable();
+$newDate = $date->setDate(2023, 2, 3);
+echo $newDate->format('d-m-y');
+
+$date = new DateTimeImmutable('2001-01-01');
+
+$newDate = $date->setTime(14, 55);
+echo $newDate->format('Y-m-d H:i:s') . "<br>";
+
+$newDate = $date->setTime(14, 55, 24);
+echo $newDate->format('Y-m-d H:i:s') . "<br>";
+
+//ADD
+$date = new DateTime('2023-01-10');
+$date->modify('+30 day');
+echo "Domani " .$date->format('d-m-Y');
+
+
+$date->modify('+3 month');
+echo "Domani " .$date->format('d-M-Y');
+
+//DIFF
+
+$origin = new DateTimeImmutable('2009-10-09');
+$target = new DateTimeImmutable('2009-10-13');
+$interval = $origin->diff($target);
+echo $interval->format('%R%a days');
