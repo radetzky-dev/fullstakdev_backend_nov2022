cb280547253bf10eaa3aa7fea7836ed9f5def483
paolo <paolo.roversi@gmail.com>
2023-01-12 19:20:06 +0100

    form

--- lezione10/altroform.php
+++ lezione10/altroform.php
@@ -2,17 +2,37 @@
 //TODO scrivere un messaggio se uno dei due valori non è valorizzato
 // creare un altro form che usa il metodo post e visualizzare i dati inviati
 
-
-if ($_GET["name"] || $_GET["age"]) {
+if ($_POST["name"] || $_POST["age"]) {
+    echo 'Chiamata POST<br>';
+    echo "Welcome " . $_POST['name'] . "<br />";
+    echo "You are " . $_POST['age'] . " years old.";
+} elseif ($_GET["name"] || $_GET["age"]) {
+    echo 'Chiamata GET<br>';
     echo "Welcome " . $_GET['name'] . "<br />";
     echo "You are " . $_GET['age'] . " years old.";
-    exit();
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    echo "Pagina appena caricata";
+} else {
+    echo "Chiamata non riconosciuta";
 }
 ?>
-<html>
-<head><title>Form</title></head>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Form</title>
+</head>
+
 <body>
+    <h2>GET</h2>
     <form action="<?php $_PHP_SELF ?>" method="GET">
+        Name: <input type="text" name="name" />
+        Age: <input type="text" name="age" />
+        <input type="submit" />
+    </form>
+    <hr>
+    <h2>POST</h2>
+    <form action="<?php $_PHP_SELF ?>" method="POST">
         Name: <input type="text" name="name" />
         Age: <input type="text" name="age" />
         <input type="submit" />
