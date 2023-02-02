3d68d84cd4b9ad1a87b05692ae07e4f9659713bc
paolo <paolo.roversi@gmail.com>
2023-02-02 18:41:08 +0100

    modfiche

--- myecommerce/inc/generateTables.php
+++ myecommerce/inc/generateTables.php
@@ -1,8 +1,14 @@
-<?php 
+<?php
 
-function showProducts($render){
-//  var_dump($render);
-$table= '<div>
+/**
+ * showProducts
+ *
+ * @param  mixed $render
+ * @return void
+ */
+function showProducts($render)
+{
+    $table = '<div>
     <table class="table table-bordered">
     <thead thead class="thead-dark">
         <tr>
@@ -15,31 +21,20 @@ $table= '<div>
         </tr>
     </thead>
    <tbody>';
-   if (is_array($render) || is_object($render)){
-    foreach ($render as $value =>$key){
-       
-return $table.=  '<tr>
-                    <td>'.$key['product_code'].'</td>
-                    <td>'.$key['product'].'</td>
-                    <td>'.$key['description'].'</td>
-                    <td>'.$key['product_q'].'</td>
-                    <td>'.$key['product_prize'].'</td>
-                    <th>'.$key['category'].'</td>
-                    
-                </tr>';
-            }
-                '</tbody>
-                </table>
-            </div>';
-   }
+   
+    if (is_array($render) || is_object($render)) {
+        foreach ($render as $value => $key) {
 
-    
+            return $table .=  '<tr>
+                    <td>' . $key['product_code'] . '</td>
+                    <td>' . $key['product'] . '</td>
+                    <td>' . $key['description'] . '</td>
+                    <td>' . $key['product_q'] . '</td>
+                    <td>' . $key['product_prize'] . '</td>
+                    <th>' . $key['category'] . '</td>
+                </tr>';
+        }
+        '</tbody></table></div>';
+    }
 }
 
-   
-           
-   
-
-
-showProducts('data/products.json');
-?>
