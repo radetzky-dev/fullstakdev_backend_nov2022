e7f4dba89a69ae6573e124b51751a1f4521dd3ca
paolo <paolo.roversi@gmail.com>
2023-02-14 21:15:12 +0100

    header

--- myecommerce/inc/navbar.php
+++ myecommerce/inc/navbar.php
@@ -5,7 +5,10 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
 
-                <?php if (!empty($_SESSION["isAdmin"])) { ?>
+                <?php
+                $logout = false;
+                if (!empty($_SESSION["isAdmin"])) {
+                    $logout = true; ?>
                     <li class="nav-item">
                         <a class="nav-link" href="insert_product.php">Inserisci prodotto</a>
                     </li>
@@ -15,18 +18,30 @@
                     <li class="nav-item">
                         <a class="nav-link" href="manage_customers.php">Gestione clienti</a>
                     </li>
-                <?php } else { ?>
+
+                <?php } elseif (!empty($_SESSION["userId"])) {
+                    $logout = true;
+                ?>
+                    <li>
+                        <div class="nav-link"><?php echo "Ciao, " . $_SESSION["userInfo"]; ?></div>
+                    </li>
 
                     <li class="nav-item">
                         <a class="nav-link" href="#">Carrello</a>
                     </li>
-                <?php } ?>
-                <li>
-                    <div class="nav-link"><?php echo "Ciao, " . $_SESSION["userInfo"]; ?></div>
+
+                <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
                     </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
-                </li>
+                <?php }
+
+                if ($logout) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                <?php } ?>
+
             </ul>
         </div>
     </div>
