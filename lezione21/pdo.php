01b152880517fe651c734dd1b984643b6c6c240b
paolo <paolo.roversi@gmail.com>
2023-02-23 18:55:04 +0100

    pdo

---
+++ lezione21/pdo.php
@@ -0,0 +1,30 @@
+<?php
+//Set env vars
+putenv("DB_HOSTNAME=localhost");
+putenv("DB_NAME=marketplacemusa");
+putenv("DB_USER=root");
+putenv("DB_PASSWORD=");
+
+print "My db is: " . getenv("DB_NAME") . "<br>";
+
+function pdoConnect()
+{
+    try {
+        return new PDO("mysql:host=" . getenv('DB_HOSTNAME') . ";dbname=" . getenv('DB_NAME') . "", getenv("DB_USER"), getenv("DB_PASSWORD"));
+    } catch (PDOException $e) {
+        echo "Errore di connessione al DB: " . $e->getMessage();
+    }
+}
+
+$db = pdoConnect();
+if ($db) {
+    echo "Connesso con successo!<br>";
+}
+
+//TODO query, insert ecc
+
+//Disconnect
+if ($db) {
+    echo "Disconnesso!";
+    $db = null;
+}
