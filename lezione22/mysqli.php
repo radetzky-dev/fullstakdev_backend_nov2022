5bf20c39bea224f6c2e8570b91b27b111e52c54a
paolo <paolo.roversi@gmail.com>
2023-02-28 20:10:30 +0100

    queries

--- lezione22/mysqli.php
+++ lezione22/mysqli.php
@@ -54,7 +54,6 @@ while ($row = $result->fetch_assoc()) {
     printf("Name: %s price %s  description %s<br>", $row["name"], $row["price"], $row["description"]);
 }
 
-
 echo "<h3>Ancora prodotti (una riga sola)</h3>";
 $query = "select * from product order by name DESC";
 $result = $conn->query($query);
@@ -62,6 +61,45 @@ $rows = $result->fetch_array(MYSQLI_NUM);
 
 var_dump($rows);
 
+echo '<hr>';
+
+/* create a prepared statement */
+$stmt = $conn->prepare("SELECT name,id FROM product WHERE id=?");
+$id = 3;
+/* bind parameters for markers */
+$stmt->bind_param("s", $id);
+/* execute query */
+$stmt->execute();
+$result = $stmt->get_result();
+$rows = $result->fetch_array(MYSQLI_NUM);
+var_dump($rows);
+
+echo '<hr>';
+
+$stmt = $conn->prepare("SELECT name,id FROM product WHERE id=?");
+$id = 1;
+$stmt->execute([$id]);
+$result = $stmt->get_result();
+$rows = $result->fetch_array(MYSQLI_NUM);
+var_dump($rows);
+
+echo '<hr>';
+$query = "SELECT name,description FROM product WHERE id=? and description=?";
+$stmt = $conn->prepare($query);
+$id = 3;
+$stmt->execute([$id,'pagnotte']);
+$result = $stmt->get_result();
+$rows = $result->fetch_array(MYSQLI_NUM);
+var_dump($rows);
+
+echo '<hr>';
+$query = "SELECT name,description FROM product WHERE id=? and description like ?";
+$stmt = $conn->prepare($query);
+$id = 3;
+$stmt->execute([$id,'%pagn%']);
+$result = $stmt->get_result();
+$rows = $result->fetch_array(MYSQLI_NUM);
+var_dump($rows);
 
 
 $conn->close();
