8571a98530ce514a7a6d721c1af51d6615187163
paolo <paolo.roversi@gmail.com>
2023-03-02 18:59:21 +0100

    saluta

--- lezione23/testclass.php
+++ lezione23/testclass.php
@@ -1,11 +1,56 @@
 <?php
 class Saluta
 {
-    public function salutoGenerico()
+    private string $name;
+
+    public string $surname ="Bianchi";
+
+    /**
+     * salutoGenerico
+     *
+     * @return void
+     */
+    public function sayHelloAnonymous(): void
     {
-        echo "Hello";
+        echo "Hello<br>";
     }
+
+    /**
+     * sayHelloWithName
+     *
+     * @return void
+     */
+    private function sayHelloWithName(): void
+    {
+        echo "Hello " . $this->name . " ".$this->surname. "<br>";
+    }
+
+    /**
+     * setName
+     *
+     * @param  mixed $name
+     * @return void
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+        $this->sayHelloWithName();
+    }
+    
+    /**
+     * getName
+     *
+     * @return void
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
 }
 
 $greeting = new Saluta;
-$greeting->salutoGenerico();
+$greeting->sayHelloAnonymous();
+$greeting->surname="Rossi";
+$greeting->setName("Mario");
+echo "Ciao ".$greeting->getName();
