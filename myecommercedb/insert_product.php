9f89ff35db92fb5725a94092ff755c708ab412d7
paolo <paolo.roversi@gmail.com>
2023-03-07 21:27:03 +0100

    category

--- myecommercedb/insert_product.php
+++ myecommercedb/insert_product.php
@@ -8,35 +8,17 @@ $action = "Inserisci prodotto";
 //se chiamata con un id lo pesca dal file e popola i campi qui sotto
 
 $db = pdoConnect();
-if ($db) {
-    try {
-        $query = 'SELECT * from category';
-        $dbStatement = getQueryResults($query, $db);
-        $result = $dbStatement->fetchAll();
-      //  var_dump($result);
-      echo "QUESTO VA NELLA SELECT<br>";
-        if (is_array($result) || is_object($result)) {
-            foreach ($result as $value => $key) {
-                echo $key['id']. ' '.$key['name'].'<br>';
-            }
-        }
-        die();
-
-    } catch (Exception $e) {
-        echo "Errore nella visualizzazione dei dati " . $e->getMessage();
-    }
-}
 
 if (!empty($_GET["id"])) {
 
     if ($db) {
         try {
-            $query = 'SELECT * from product where id='.$_GET["id"];
+            $query = 'SELECT * from product where id=' . $_GET["id"];
             $dbStatement = getQueryResults($query, $db);
             $result = $dbStatement->fetchAll();
 
-           // var_dump($result[0]['name']);
-           $product =$result[0]['name']; //Todo resto del mapping
+            // var_dump($result[0]['name']);
+            $product = $result[0]['name']; //Todo resto del mapping
 
             //TODO mappare i campi
 
@@ -55,32 +37,69 @@ if (!empty($_GET["id"])) {
         <h3><?php echo $action; ?></h3>
 
         <form action="savedata.php" method="post" enctype="multipart/form-data">
-        
-        <input name="product_id" id="product_id" type="hidden" value="<?php if (!empty($_GET["id"])) { echo $_GET["id"]; }?>" class="form-control">
+
+        <input name="product_id" id="product_id" type="hidden" value="<?php if (!empty($_GET["id"])) {echo $_GET["id"];}?>" class="form-control">
             <div class="mb-3">
                 <label for="product" class="form-label">Nome prodotto</label>
-                <input name="product" id="product" type="text" value="<?php if (!empty($product)) echo $product; ?>" class="form-control" required>
+                <input name="product" id="product" type="text" value="<?php if (!empty($product)) {
+    echo $product;
+}
+?>" class="form-control" required>
             </div>
-           
+
             <div class="mb-3">
                 <label for="product_q" class="form-label">Quantità</label>
-                <input name="product_q" id="product_q" type="number" value="<?php if (!empty($currentProduct)) echo $currentProduct['product_q'] ?>" class="form-control" required>
+                <input name="product_q" id="product_q" type="number" value="<?php if (!empty($currentProduct)) {
+    echo $currentProduct['product_q'];
+}
+?>" class="form-control" required>
             </div>
             <div class="mb-3">
                 <label for="product_prize" class="form-label">Prezzo</label>
-                <input name="product_prize" id="product_prize" type="number" step="0.1" value="<?php if (!empty($currentProduct)) echo $currentProduct['product_prize'] ?>" class="form-control" required>
+                <input name="product_prize" id="product_prize" type="number" step="0.1" value="<?php if (!empty($currentProduct)) {
+    echo $currentProduct['product_prize'];
+}
+?>" class="form-control" required>
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <input name="description" id="description" type="text" value="<?php if (!empty($currentProduct)) echo $currentProduct['description'] ?>" class="form-control" required>
+                <input name="description" id="description" type="text" value="<?php if (!empty($currentProduct)) {
+    echo $currentProduct['description'];
+}
+?>" class="form-control" required>
             </div>
             <div class="mb-3">
                 <label for="category" class="form-label">Categoria</label>
-                <input name="category" id="category" type="text" value="<?php if (!empty($currentProduct)) echo $currentProduct['category'] ?>" class="form-control" required>
+                <?php
+if ($db) {
+    try {
+        $query = 'SELECT * from category';
+        $dbStatement = getQueryResults($query, $db);
+        $result = $dbStatement->fetchAll();
+        //  var_dump($result);
+        echo "QUESTO VA NELLA SELECT<br>";
+        if (is_array($result) || is_object($result)) {
+            foreach ($result as $value => $key) {
+                echo $key['id'] . ' ' . $key['name'] . '<br>';
+            }
+        }
+
+    } catch (Exception $e) {
+        echo "Errore nella visualizzazione dei dati " . $e->getMessage();
+    }
+}
+?>
+                <input name="category" id="category" type="text" value="<?php if (!empty($currentProduct)) {
+    echo $currentProduct['category'];
+}
+?>" class="form-control" required>
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Aggiungi immagine</label>
-                <input name="image" id="image" type="file" value="<?php if (!empty($currentProduct)) echo $currentProduct['image'] ?>" class="form-control" required>
+                <input name="image" id="image" type="file" value="<?php if (!empty($currentProduct)) {
+    echo $currentProduct['image'];
+}
+?>" class="form-control" required>
             </div>
             <div class="mb-3">
                 <div>
@@ -92,7 +111,7 @@ if (!empty($_GET["id"])) {
 </container>
 
 
-<?php 
-        $db = null;
+<?php
+$db = null;
 
-include "inc/footer.php"; ?>
+include "inc/footer.php";?>
