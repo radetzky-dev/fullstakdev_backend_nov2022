c62918101ac8773c4f6942937a15cd9ae6ac23ae
paolo <paolo.roversi@gmail.com>
2023-03-23 20:59:26 +0100

    controller

---
+++ student/app/Http/Controllers/StudentController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $student = Student::all();
+        return view('index', compact('student'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $storeData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'phone' => 'required|numeric',
+            'password' => 'required|max:255',
+        ]);
+        $student = Student::create($storeData);
+        return redirect('/students')->with('completed', 'Student has been saved!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $student = Student::findOrFail($id);
+        return view('edit', compact('student'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $updateData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'phone' => 'required|numeric',
+            'password' => 'required|max:255',
+        ]);
+        Student::whereId($id)->update($updateData);
+        return redirect('/students')->with('completed', 'Student has been updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $student = Student::findOrFail($id);
+        $student->delete();
+        return redirect('/students')->with('completed', 'Student has been deleted');
+    }
+}
--- student/database/migrations/2023_03_23_194311_create_students_table.php
+++ student/database/migrations/2023_03_23_194311_create_students_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('password');
             $table->timestamps();
         });
     }
