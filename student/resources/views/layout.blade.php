ab0495616d57147c344147c7e0d8d8c25e7f9ab7
paolo <paolo.roversi@gmail.com>
2023-03-28 19:12:11 +0200

    titolo

--- student/resources/views/layout.blade.php
+++ student/resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 7/6 CRUD App Example</title>
+    <title>Students</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 
