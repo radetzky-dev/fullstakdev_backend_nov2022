6da87b52d644791892a96f8ea5d8feb87406405b
bru8787 <bruno.ndini@gmail.com>
2023-03-30 00:56:00 +0200

    bruno

--- student/app/Services/CreatePdf.php
+++ student/app/Services/CreatePdf.php
@@ -7,10 +7,13 @@ class CreatePdf
 {
     public static function createPDF() 
     {
+        
         $result = Student::all();
+        Pdf::loadHTML($result)->setPaper('a4', 'landscape');
         $student=$result->toArray();
         view()->share('student',$student);
+        Pdf::setOption(['dpi' => 150, 'defaultFont' => 'Times-BoldItalic']);
         $pdf = PDF::loadView('pdf_view', $student);
-        return $pdf->download('pdf_file.pdf');
+        return $pdf->stream('pdf_file.pdf');
     }
 }
