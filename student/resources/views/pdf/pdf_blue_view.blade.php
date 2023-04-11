3860aefcd436b9fed215df76ce559c1b34b5a713
paolo <paolo.roversi@gmail.com>
2023-04-11 20:21:28 +0200

    rotte

--- student/resources/views/pdf/pdf_blue_view.blade.php
+++ student/resources/views/pdf/pdf_blue_view.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> PDF File</title>
+    <title>PDF File</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 
