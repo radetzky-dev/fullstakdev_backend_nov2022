21096a1bc61670127bc46b9862ec7ec4aaf8ec80
paolo <paolo.roversi@gmail.com>
2023-05-09 18:57:02 +0200

    seeder

---
+++ student/database/seeders/StudentSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('students')->insert([
+            'name' => 'SIG.'.Str::random(10),
+            'email' => Str::random(10).'@yahoo.com',
+            'phone' => rand(10000, 5000000),
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
