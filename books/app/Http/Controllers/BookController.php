63b6c770766432f2acb4a570512f144c81a975f6
paolo <paolo.roversi@gmail.com>
2023-06-01 18:42:00 +0200

    book

--- books/app/Http/Controllers/BookController.php
+++ books/app/Http/Controllers/BookController.php
@@ -56,9 +56,9 @@ class BookController extends Controller
     public function update(Request $request, Book $book)
     {
         // check if currently authenticated user is the owner of the book
-        if ($request->user()->id !== $book->user_id) {
+       /* if ($request->user()->id !== $book->user_id) {
             return response()->json(['error' => 'You can only edit your own books.'], 403);
-        }
+        }*/
         $book->update($request->only(['title', 'description']));
 
         return new BookResource($book);
